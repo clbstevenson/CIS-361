33bec2ff56e0f4c00d087e8afafe725ea1c4b2ba
Caleb Stevenson <stevecal@mail.gvsu.edu>
2016-02-17 21:49:39 -0500

    Added decrypt.c file

---
+++ proj1/decrypt.c
@@ -0,0 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Load array 'given' with letter frequences for English from LetFreq.txt 
+void readFreq(float given[], char fname[]) {
+
+}
+
+// Read the encoded text from an input file and accumulate letter frequency 
+// data for the encoded text. Store the frequency data in array found.
+void calcFreq ( float found[]  char fname[] ) {
+
+}
+ 
+// Rotate the character in parameter ch down the alphabet for the number of 
+// positions as given in parameter num and return the resulting character.
+char rotate ( char ch, int num ) {
+
+}
+
+// Compare the data in array found with frequency data in given[], looking
+// for a key that will give you the best match. To do this, try each of the
+// 26 rotations, and remember which gives the smallest difference between
+// the frequencies you observed and the frequencies given. Return the key.
+int findKey ( float given[], float found[] ) {
+
+}
+ 
+// Decrypt the encoded text in the input file using the key and 
+// display the decoded text
+void decrypt ( int key  char fname[]  ) {
+
+}
+
+int main() {
+
+    return 0;
+}
