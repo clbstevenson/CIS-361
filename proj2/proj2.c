381e8f5277ae74b6b6320585231bf9bd5e25c47f
Caleb Stevenson <stevecal@mail.gvsu.edu>
2016-03-23 19:21:45 -0400

    Added expdist code from project description

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <queue.h>
-#include <stats.h>
+#include <math.h>
+//#include <queue.h>
+//#include <stats.h>
+
+#define AVG_SERVICE 2.0
 
 void simulation(int numOfTellers);
 
+double expdist (double mean) {
+    //TODO improve random
+    double r = rand();
+    r /= RAND_MAX;
+    return -mean * log(r);
+}
+
 int main(int argc, char* argv[]) {
+
+    double t;
+    // ...
+    t = expdist (AVG_SERVICE);
+    printf("t:%f\n", t);
+    // ..
 
 }
 
